daae557957f86ebebcebfcb38797402c6ac33e14
anysong <985774505@qq.com>
2018-09-02 18:22:38 +0800

    dev

---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+import 'font-awesome/css/font-awesome.min.css';
+import './index.css';
+import './index.scss';
+
+
+ReactDOM.render(
+	<div>
+		<h1>hiiiaafff</h1>
+		<i className="fa fa-address-book"></i>
+	</div>,
+	document.getElementById('root')
+);
+
---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+    entry: './src/app.jsx',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist/',
+        filename: 'js/app.js'
+    },
+    module: {
+        rules: [{
+            test: /\.jsx$/,
+            exclude: /(node_modules|bower_components)/, //该目录下文件不做处理
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env', 'react'] //根据环境打包 浏览器／node
+                }
+            }
+        }, {
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: "css-loader"
+            })
+        }, {
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: ['css-loader', 'sass-loader']
+            })
+        }, {
+            test: /\.(png|jpg|gif)$/i,
+            use: [{
+                loader: 'url-loader',
+                options: {
+                    limit: 8192,
+                    name: 'resource/[name].[ext]'
+                }
+            }]
+        }, {
+            test: /\.(woff|svg|woff2|eot|ttf|otf)$/,
+            use: [{
+                loader: 'url-loader',
+                options: {
+                    limit: 8192,
+                    name: 'resource/[name].[ext]'
+                }
+            }]
+        }]
+    },
+    plugins: [
+
+        new HtmlWebpackPlugin({
+            template: './src/index.html' //引模版
+        }),
+        //独立css
+        new ExtractTextPlugin("css/[name].css"),
+        //提出公共模块
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'common',
+            filename: 'js/base.js'
+        })
+    ],
+    devServer: {
+        compress: true,
+        port: 9000
+    }
+};
